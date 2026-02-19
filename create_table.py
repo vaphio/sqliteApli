da05273d8aa24e3e6ecfa11140c500f7acaacb63
vaphio <mura-shin@nifty.com>
2026-02-19 21:04:48 +0900

    bugfix

--- create_table.py
+++ create_table.py
@@ -2,13 +2,14 @@
 # need 2 arguments for database file and table name
 
 import sqlite3
+import sys
 
 dbpath = sys.argv[1]
 conn = sqlite3.connect(dbpath)
 
 cursor = conn.cursor()
-sql = '''CREATE TABLE IF NOT EXISTS''' + sys.argv[2] +
-    ''' (
+sql = '''CREATE TABLE IF NOT EXISTS ''' + sys.argv[2]
+sql = sql +  ''' (
      id TEXT PRIMARY KEY,
      name TEXT,
      auther TEXT,
